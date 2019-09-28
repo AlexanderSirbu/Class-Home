fb44bfeb3fcc1ee6789ab159b05aba6553f5e62e
Alexander <sirbualexandru92@gmail.com>
2019-09-28 15:56:17 +0300

    classwork

---
+++ ClassWork/(28.09.2019)JavaScript/script.js
@@ -0,0 +1,16 @@
+var helloWorld = "Hello World!";
+var today = "Saturday";
+
+if (today === "Saurday") {
+    console.log("True");
+} else {
+    var today = "Sat";
+    console.log("New variables " + today);
+}
+console.log(typeof today);
+
+
+console.log(helloWorld);
+console.log(helloWorld + " My first app");
+console.log(helloWorld + " Second app");
+console.log(helloWorld + " " + helloWorld);
