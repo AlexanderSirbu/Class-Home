aad6c041ee70ecac122bf56a09e672fda453a245
Alexander <sirbualexandru92@gmail.com>
2019-09-29 16:27:52 +0300

    end home work

--- HomeWork/(29.09.2019)Ifandswitch/js/switch.js
+++ HomeWork/(29.09.2019)Ifandswitch/js/switch.js
@@ -1,17 +1,39 @@
-var work = prompt("Write any day of the week")
+document.getElementById("callif").onclick = function () {
+    var work = prompt("Write any day of the week")
 
-switch(work){
-    case "Monday":
-    case "Tuesday":
-    case "Wednesday":
-    case "Thursday":
-    case "Friday":        
-        alert("It's a working day");
-        break;
-    case"Saturday":
-    case"Sunday":
-        alert("It's a weekend day")
-        break;
-    default:
-        alert("Can not define,please try another value!");
+    switch(work){
+        case "Monday":
+        case "Tuesday":
+        case "Wednesday":
+        case "Thursday":
+        case "Friday":        
+            alert("It's a working day");
+            break;
+        case"Saturday":
+        case"Sunday":
+            alert("It's a weekend day")
+            break;
+        default:
+            alert("Can not define,please try another value!");
+    }  
+}
+
+document.getElementById("callswitch").onclick = function () {
+    var work = prompt("Write any day of the week")
+
+    switch(work){
+        case "Monday":
+        case "Tuesday":
+        case "Wednesday":
+        case "Thursday":
+        case "Friday":        
+            alert("It's a working day");
+            break;
+        case"Saturday":
+        case"Sunday":
+            alert("It's a weekend day")
+            break;
+        default:
+            alert("Can not define,please try another value!");
+    }  
 }
