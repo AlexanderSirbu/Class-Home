0c03172d44600f2b8e85ed38ca3f3e421fee8060
Alexander <sirbualexandru92@gmail.com>
2019-10-05 18:53:55 +0300

    end

--- HomeWork/(05.10.2019)ObjectandArray/javascript.js
+++ HomeWork/(05.10.2019)ObjectandArray/javascript.js
@@ -1,7 +1,30 @@
 var test = ["HTML", 11, "JavaScript", 2, "CSS", 23, 12];
+console.log(test);
+
 test.push(-11);
+console.log(test);
+
 test.unshift(1000);
+console.log(test);
+
 test.push(["Last array element"]);
+console.log(test);
+
 test.splice(3, 1);
+console.log(test);
 
+test = test.filter(item =>{return  item === +item});
+console.log(test);
 
+newtest = test.map(item => Math.pow(item, 2));
+console.log(newtest);
+
+var max = newtest[0];
+for(i = 0; i < newtest.length; i++){
+    if(newtest[i] >= max){
+        max = newtest[i];
+    }
+};
+console.log(max);
+//let,cost
+//+item
