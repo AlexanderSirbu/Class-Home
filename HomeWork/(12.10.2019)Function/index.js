f52c18464260627f7bba844165ea7ada598f82f0
Alexander <sirbualexandru92@gmail.com>
2019-10-12 20:35:10 +0300

    +

--- HomeWork/(12.10.2019)Function/index.js
+++ HomeWork/(12.10.2019)Function/index.js
@@ -1,25 +1,27 @@
 var a = parseInt(prompt("Введите коэфициент a = "));
 var b = parseInt(prompt("Введите коэфициент b = "));
 var c = parseInt(prompt("Введите коэфициент c = "));
-var D, x1, x2;
+
 
 function getDescriminant() {
-    D = Math.pow(b, 2) - 4 * a * c;
-    console.log("Дискриминант : " + D);
+    var D = Math.pow(b, 2) - 4 * a * c;
+   
+    return D;
 };
+    var D = console.log("Дискриминант : " + getDescriminant());
+    console.log(D);
 
 function rootsOfQuadraticEquation() {
     if (D < 0) {
         alert("нет действительных корней");
         console.log("нет действительных корней");
-
     } else if (D == 0) {
-        x1 = -b / (2 * a);
+        var x1 = -b / (2 * a);
         alert("Дискриминант : " + D + ", корень уровнения :" + x1);
         console.log("Дискриминант : " + D + ", корень уровнения :" + x1);
     } else if (D > 0) {
-        x1 = (-b + Math.sqrt(D)) / (2 * a);
-        x2 = (-b - Math.sqrt(D)) / (2 * a);
+        var x1 = (-b + Math.sqrt(D)) / (2 * a);
+        var x2 = (-b - Math.sqrt(D)) / (2 * a);
         alert("Дискриминант : " + D + " , корень уровнения x1 : " + x1 + ", корень уровнения  x2 : " + x2);
         console.log("Дискриминант : " + D + " , корень уровнения x1 : " + x1 + ", корень уровнения  x2 : " + x2);
 
@@ -27,5 +29,7 @@ function rootsOfQuadraticEquation() {
         console.log("err");
     }
 };
-getDescriminant();
+
 rootsOfQuadraticEquation();
+
+
