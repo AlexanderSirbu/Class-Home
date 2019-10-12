4a8696fce07374a366e5b37e50a5a13499d6d191
Alexander <sirbualexandru92@gmail.com>
2019-10-12 17:15:51 +0300

    +

---
+++ ClassWork/12.10/index.js
@@ -0,0 +1,53 @@
+// function name(){
+//     var a = 5;
+// }
+// console.log(name);
+
+// console.log("Lesson 15");
+
+var a = 10;
+
+console.log(a);
+
+// console.log(sum(1, 5));
+
+// const objectForSumm = {
+//     a: 5,
+//     b: 6
+// };
+
+// function summ(objectForSumm){
+    
+//     return objectForSumm.a + objectForSumm.b;
+// };
+// console.log(summ(objectForSumm));
+// function calculateTax(a, b){
+//     if(a === undefined || b === undefined){
+//         console.log("Please enter corectly");
+//         return ;
+//     }
+//     return a+b;
+// }
+// console.log(calculateTax(undefined, 2));
+
+
+
+// var number = [1,2,3,4,5,8];
+
+
+// var result = number.filter(function(value, index){
+// return value > 4;
+
+// });
+
+
+// console.log(result);
+// function summ(a = 20, b){
+    
+//     return a + b;
+// };
+//  var sum = function (a, b){
+//     return a + b;
+// };
+
+
---
+++ HomeWork/(12.10.2019)Function/index.js
@@ -0,0 +1,30 @@
+// function main(){
+//     var a = 2+2;
+
+// }
+// console.log(main);
+function main() {
+    var a = +prompt("Введите коэфициент a = ");
+    var b = +prompt("Введите коэфициент b = ");
+    var c = +prompt("Введите коэфициент c = ");
+    var x1, x2;
+    var D = Math.pow(b, 2) - 4 * a * c;
+
+    if (a == 0 && a == " " && a != null) {
+        a = 1;
+    } else if (D < 0) {
+        console.log("нет действительных корней");
+    } else if (D == 0) {
+        x1 = -b / (2 * a);
+        console.log("Дискриминант : " + D + ", корень уровнения :" + x1);
+    } else if (D > 0) {
+        x1 = (-b + Math.sqrt(D)) / (2 * a);
+        x2 = (-b - Math.sqrt(D)) / (2 * a);
+        console.log("Дискриминант : " + D + " , корень уровнения x1 : " + x1 + ", корень уровнения  x2 : " + x2);
+    } else {
+        console.log("Произошла ошибка,введите еше один раз")
+    };
+    
+};
+console.log(main());
+//console.log(main(22,33,66));
