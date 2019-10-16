3aa1196e566ab6fcf02bb9d024bb5717a205cc87
Alexander <sirbualexandru92@gmail.com>
2019-10-16 15:45:44 +0300

    eslint

--- HomeWork/(12.10.2019)Function/index.js
+++ HomeWork/(12.10.2019)Function/index.js
@@ -1,16 +1,13 @@
-var a = parseInt(prompt("Введите коэфициент a = "));
-var b = parseInt(prompt("Введите коэфициент b = "));
-var c = parseInt(prompt("Введите коэфициент c = "));
+var a = parseInt(prompt('Введите коэфициент a = '));
+var b = parseInt(prompt('Введите коэфициент b = '));
+var c = parseInt(prompt('Введите коэфициент c = '));
 
+function getDescriminant () {
+  // return D;
+  // can write like this
+  return Math.pow(b, 2) - 4 * a * c;
+}
 
-function getDescriminant() {
-    
-
-    // return D;
-    // can write like this
-    return Math.pow(b, 2) - 4 * a * c
-   
-};
 
 // we can now call our function and assign result to value
 var descriminant = getDescriminant();
@@ -22,22 +19,21 @@ rootsOfQuadraticEquation(descriminant);
 
 // until here
 
-function rootsOfQuadraticEquation(descriminant) {
-    if (descriminant < 0) {
-        alert("нет действительных корней");
-        console.log("нет действительных корней");
-        // here we need to return nothing, just simple 'return;'
-    } else if (descriminant == 0) {
-        var x1 = -b / (2 * a);
-        alert("Дискриминант : " + descriminant + ", корень уровнения :" + x1);
-        console.log("Дискриминант : " + descriminant + ", корень уровнения :" + x1);
-    } else if (descriminant > 0) {
-        var x1 = (-b + Math.sqrt(descriminant)) / (2 * a);
-        var x2 = (-b - Math.sqrt(descriminant)) / (2 * a);
-        alert("Дискриминант : " + descriminant + " , корень уровнения x1 : " + x1 + ", корень уровнения  x2 : " + x2);
-        console.log("Дискриминант : " + descriminant + " , корень уровнения x1 : " + x1 + ", корень уровнения  x2 : " + x2);
-
-    } else {
-        console.log("err");
-    }
-};
+function rootsOfQuadraticEquation (descriminant) {
+  if (descriminant < 0) {
+    alert('нет действительных корней');
+    console.log('нет действительных корней');
+    // here we need to return nothing, just simple 'return;'
+  } else if (descriminant == 0) {
+    var x1 = -b / (2 * a);
+    alert("Дискриминант : " + descriminant + ', корень уровнения :' + x1);
+    console.log('Дискриминант : ' + descriminant + ', корень уровнения :' + x1);
+  } else if (descriminant > 0) {
+    x1 = (-b + Math.sqrt(descriminant)) / (2 * a);
+    var x2 = (-b - Math.sqrt(descriminant)) / (2 * a);
+    alert('Дискриминант : ' + descriminant + ' , корень уровнения x1 : ' + x1 + ', корень уровнения  x2 : ' + x2);
+    console.log('Дискриминант : ' + descriminant + ' , корень уровнения x1 : ' + x1 + ', корень уровнения  x2 : ' + x2);
+  } else {
+    console.log('err');
+  }
+}
