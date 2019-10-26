e39d7ae6465a1a7e663bc3be2a745933533e45ce
Alexander <sirbualexandru92@gmail.com>
2019-10-26 17:44:18 +0300

    +

--- HomeWork/HomeWork_19(27.10)/index.js
+++ HomeWork/HomeWork_19(27.10)/index.js
@@ -1,14 +1,13 @@
 function changeValueFirstBtn(){
-    document.getElementById('paragraph').innerText = "This is action first-btn";
-    setTimeout(() =>alert('When u click me i change text '),0);
+    document.getElementById('paragraph').innerText = "You clicked First button";
+    
 };
 function changeValueSecondBtn(){
-    document.getElementById('paragraph').innerText = "This is action third-btn";
-    setTimeout(() =>alert(' When u click me with right btn  i change text'),0);
+    document.getElementById('paragraph').innerText = "You clicked Second button";
+    
 };
 function changeValueThirdBtn(){
-    document.getElementById('paragraph').innerText = "This is action second-btn";
-    setTimeout(() =>alert('When u hover me i change text '),0);
+    document.getElementById('paragraph').innerText = "You clicked Third button";
 };
 
 
