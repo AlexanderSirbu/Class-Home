f5259411adcc8783c51ec88762658c1ab62ae9c5
Alexander <sirbualexandru92@gmail.com>
2019-10-28 10:29:19 +0200

    homework

--- HomeWork/HomeWork_20(02.11)/index.js
+++ HomeWork/HomeWork_20(02.11)/index.js
@@ -1,19 +1,26 @@
-const btnBlock = document.getElementById('btn-block');
+const btnBlock = document.getElementById('btn-main');
 const paragraph = document.getElementById('paragraph');
-
+const elems = document.querySelectorAll('*');
 
 
 function test(event) {
+
     if (event.target.classList.contains('first')) {
-        paragraph.innerText = "You clicked First button";        
+        paragraph.innerText = "You clicked First button";
         console.log("capturing", this.tagName);
-    }else if(event.target.classList.contains('second')){
-        paragraph.innerText = "You clicked Second button"; 
+
+    } else if (event.target.classList.contains('second')) {
+        paragraph.innerText = "You clicked Second button";
         console.log("capturing", this.tagName);
-    }else if(event.target.classList.contains('third')){
-        paragraph.innerText = "You clicked Third button"; 
+
+    } else if (event.target.classList.contains('third')) {
+        paragraph.innerText = "You clicked Third button";
         console.log("capturing", this.tagName);
     }
+
 };
 
-btnBlock.addEventListener('click', test);
+for (var i = 0; i < elems.length; i++) {
+    elems[i].addEventListener("click", test, true);
+}
+btnBlock.addEventListener("click", test, true)
