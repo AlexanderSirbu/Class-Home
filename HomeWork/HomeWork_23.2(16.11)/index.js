59af278bb4269d7b0b4bd724eecdd7f6add4c6bb
Alexander <sirbualexandru92@gmail.com>
2019-11-11 16:06:58 +0200

    hw

--- HomeWork/HomeWork_23.2(16.11)/index.js
+++ HomeWork/HomeWork_23.2(16.11)/index.js
@@ -14,21 +14,35 @@ function verificateLocalStorage() {
 
 };
 
+// function repeatRegister() {
+//   let name = prompt('What is ur name?');
+//   if (name.length >= 2) {
+//     localStorage.setItem('name', name);
+//   } else {
+//     alert('name need to be longer ')
+//     //  repeatRegister();
+//   }
+// }
+// };
 
 function main(event) {
 
   if (event.target.classList.contains('register')) {
-    let name = prompt('What is ur name?');
-    if (name.length >= 2) {
-      localStorage.setItem('name', name);
-    } else {
-      alert('name must be longer');
+    // repeatRegister();
+    for (let i = 0; name.length < 2; i++) {
+      let name = prompt('What is ur name?');
+      if (name.length >= 2) {
+        localStorage.setItem('name', name);
+        break;
+      } else {
+        alert('name need to be longer ')
+      }
     };
-    document.querySelector('p').textContent = 'Hello ' + localStorage.getItem('name');
-  } else if (event.target.classList.contains('reset')) {
-    localStorage.removeItem('name');
-    document.querySelector('p').textContent = 'Hello guest!'
-  }
+  document.querySelector('p').textContent = 'Hello ' + localStorage.getItem('name');
+} else if (event.target.classList.contains('reset')) {
+  localStorage.removeItem('name');
+  document.querySelector('p').textContent = 'Hello guest!'
+}
 
 };
 
