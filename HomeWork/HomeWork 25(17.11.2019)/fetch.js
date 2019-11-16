b40c7b66e0225830d05956fc4466ad2c8fca2ee8
Alexander <sirbualexandru92@gmail.com>
2019-11-16 22:06:23 +0200

    +

--- HomeWork/HomeWork 25(17.11.2019)/fetch.js
+++ HomeWork/HomeWork 25(17.11.2019)/fetch.js
@@ -4,42 +4,71 @@ const nasaCopyright = document.querySelector(".nasa__copyright")
 const nasaExplanation = document.querySelector(".nasa__explanation")
 const nasaDate = document.querySelector(".nasa__date")
 const btnPrev = document.querySelector(".prev")
+const btnNext = document.querySelector(".next")
+let URL = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+const today = new Date();
+let date = today.getDate();
+let month = today.getMonth() + 1;
+let fullDate = `${today.getFullYear()}-${month}-${date}`
 
-let nasaURL = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+getPictureOfDay(URL);
 
+function getPictureOfDay(URL) {
 
-fetch(nasaURL)
-  .then(response => {
+  fetch(URL)
+    .then(response => {
       return response.json()
     })
-    .then(result =>{
-        console.log(result)
-        const {url, copyright, title, explanation, date} = result;
-        nasaImage.src = url
-        nasaTitle.innerHTML = title;
-        nasaExplanation.innerHTML = explanation;
-        nasaCopyright.innerHTML = copyright;
-        nasaDate.innerHTML = date;
-      });
-  
+    .then(result => {
+      console.log(result)
+      const {
+        url,
+        copyright,
+        title,
+        explanation,
+        date
+      } = result;
+      nasaImage.src = url
+      nasaTitle.innerHTML = title;
+      nasaExplanation.innerHTML = explanation;
+      nasaCopyright.innerHTML = copyright;
+      nasaDate.innerHTML = date;
+    });
 
-    
-    const today = new Date();
-    let date = today.getDate();
-    let month = today.getMonth()+1;
-    let fullDate = `${today.getFullYear()}-${month}-${date}`
-
-    function prevDay(){
-      console.log('btn')
-      
-      fullDate = `${today.getFullYear()}-${month}-${--date}`
-      nasaURL = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${fullDate}`
-
-      if(date <= 1){
-        date = 31;
-        --month;
-        console.log('its ok')
-      }
-  
 }
-btnPrev.addEventListener('click',prevDay)
+
+function prevDay() {
+
+  fullDate = `${today.getFullYear()}-${month}-${--date}`
+  nasaURL = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${fullDate}`
+  if (date <= 1) {
+    date = 31;
+    --month;
+  }
+
+  getPictureOfDay(nasaURL)
+}
+
+function nextDay() {
+
+  if (date == today.getDate()) {
+    console.log('this is future')
+  } else {
+    fullDate = `${today.getFullYear()}-${month}-${++date}`
+    nasaURL = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${fullDate}`
+    if (date >= 30) {
+      date = 0;
+      ++month;
+    }
+  }
+
+  getPictureOfDay(nasaURL)
+}
+
+
+
+
+
+
+btnPrev.addEventListener('click', prevDay)
+btnNext.addEventListener('click', nextDay)
